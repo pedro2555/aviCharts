55aa4d46405474da10fe16b28a757f913542cbd5
Pedro Rodrigues <prodrigues1990@gmail.com>
2017-01-22 02:42:23 +0000

    Initial commit

--- AviCharts/mainFrm.cs
+++ AviCharts/mainFrm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AviCharts
 {
-    public partial class Form1 : Form
+    public partial class mainFrm : Form
     {
-        public Form1()
+        public mainFrm()
         {
             InitializeComponent();
         }
