fc7f93faa5ac7f330efbf8bc50fca0b67ab5311d
Pedro Rodrigues <prodrigues1990@gmail.com>
2017-01-22 16:21:54 +0000

    Working, super basic.

--- AviCharts/mainFrm.Designer.cs
+++ AviCharts/mainFrm.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainFrm));
+            this.pdfViewer = new PdfiumViewer.PdfViewer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // pdfViewer
+            // 
+            this.pdfViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer.Location = new System.Drawing.Point(0, 0);
+            this.pdfViewer.Name = "pdfViewer";
+            this.pdfViewer.ShowToolbar = false;
+            this.pdfViewer.Size = new System.Drawing.Size(845, 461);
+            this.pdfViewer.TabIndex = 0;
+            this.pdfViewer.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitBest;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(211, 461);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pdfViewer);
+            this.splitContainer1.Size = new System.Drawing.Size(1060, 461);
+            this.splitContainer1.SplitterDistance = 211;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // mainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1060, 461);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainFrm";
+            this.Text = "AviCharts";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PdfiumViewer.PdfViewer pdfViewer;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- AviCharts/mainFrm.cs
+++ AviCharts/mainFrm.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PdfiumViewer;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace AviCharts
@@ -15,6 +10,62 @@ namespace AviCharts
         public mainFrm()
         {
             InitializeComponent();
+
+            PopulateTreeView();
+        }
+
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+
+            DirectoryInfo info = new DirectoryInfo(Environment.CurrentDirectory + "\\CHARTS");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                treeView1.Nodes.Add(rootNode);
+                rootNode.Expand();
+            }
+
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+
+                GetFiles(subDir, aNode);
+
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        private void GetFiles(DirectoryInfo dir, TreeNode nodeToAdd)
+        {
+            foreach (FileInfo file in dir.GetFiles("*.pdf"))
+            {
+                nodeToAdd.Nodes.Add(
+                    file.FullName,
+                    file.Name.Substring(file.Name.IndexOf('.') + 1, file.Name.Length - file.Name.IndexOf('.') - 5));
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                pdfViewer.Document = PdfDocument.Load((e.Node.Nodes.Count == 0) ? e.Node.Name : e.Node.Nodes[0].Name);
+                pdfViewer.Focus();
+            }
+            catch (Exception crap)
+            {
+                pdfViewer.Document = null;
+            }
         }
     }
 }
